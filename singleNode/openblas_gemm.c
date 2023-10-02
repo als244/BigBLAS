7ee6e3626c5837b119e561541c944252dba21a81
Andrew Sheinberg <andrew.sheinberg@gmail.com>
2023-10-02 15:13:49 -0400

    example calling openblas GEMM

---
+++ singleNode/openblas_gemm.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <sys/time.h>
+#include <cblas.h>
+
+#define M_PI 3.14159265358979323846
+
+void sampleNormal(float * X, int size, float mean, float var) {
+	float x, y, z, std, val;
+	for (int i = 0; i < size; i++){
+		x = (float)rand() / RAND_MAX;
+		y = (float)rand() / RAND_MAX;
+		z = sqrtf(-2 * logf(x)) * cosf(2 * M_PI * y);
+		std = sqrtf(var);
+		val = std * z + mean;
+		X[i] = val;
+	}
+}
+
+int main(int argc, char * argv[]){
+
+	if (argc != 5){
+		fprintf(stderr, "Wrong number of args\n");
+		exit(1);
+	}
+
+	size_t m = atol(argv[1]);
+	size_t k = atol(argv[2]);
+	size_t n = atol(argv[3]);
+
+	int num_threads = atoi(argv[4]);
+
+	if (m <= 0 || k <= 0 || n <= 0){
+		fprintf(stderr, "Bad dimensions\n");
+		exit(1);
+	}
+
+	float * A = calloc(m * k, sizeof(float));
+	float * B = calloc(k * n, sizeof(float));
+	float * C = calloc(m * n, sizeof(float));
+
+	if ((A == NULL) || (B == NULL) || (C == NULL)){
+		fprintf(stderr, "OUT OF MEMORY!\n");
+		exit(1);
+	}
+
+	printf("SETTING MATRICES!\n");
+	sampleNormal(A, m * k, 0.0, 1.0 / 20.0);
+	sampleNormal(B, k * n, 0.0, 1.0 / 20.0);
+
+
+	openblas_set_num_threads(num_threads);
+
+	printf("DOING MATMUL!\n\n");
+
+	struct timeval  tv1, tv2;
+    	gettimeofday(&tv1, NULL);
+	cblas_sgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, 
+					m, n, k, 1.0, A, k, B, n, 
+					0.0, C, n);
+  	gettimeofday(&tv2, NULL);
+    	double time_taken = (double) (tv2.tv_usec - tv1.tv_usec) / 1000000 +
+         (double) (tv2.tv_sec - tv1.tv_sec); // in seconds
+
+    	printf("SGEMM where: m=%zu, k=%zu, n=%zu took --- %f seconds\n", m, k, n, time_taken);
+    	printf("GFLOPS Proxy: %f\n", 2 * (double) m * (double) k * (double) n / time_taken / 1e9); 
+
+    	free(A);
+    	free(B);
+    	free(C);
+
+    	return 0;
+
+}
