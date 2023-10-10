2a2041edb59cb7668f3811fd575563c90f624bae
Andrew Sheinberg <andrew.sheinberg@gmail.com>
2023-10-10 18:47:27 -0400

    making asimple protocol that sends entire input in 1 message (to test gRPC working correctly)

---
+++ clientServer/python_client/gemm_client_simple.py
@@ -0,0 +1,68 @@
+import sys
+import logging
+import random
+import numpy as np
+
+import grpc
+import gemm_simple_pb2
+import gemm_simple_pb2_grpc
+
+
+def createMatricesInputMessage(operationId, A, B):
+	m = A.shape[0]
+	k = A.shape[1]
+	n = B.shape[1]
+
+	matrixData = np.ravel(A).tolist() + np.ravel(B).tolist()
+
+	matricesInputMessage = gemm_simple_pb2.MatricesInput(
+		operationId = operationId,
+		m = m,
+		k = k,
+		n = n
+	)
+
+	matricesInputMessage.data[:] = matrixData
+	return matricesInputMessage
+
+def do_rpc_matmul(stub, A, B):
+
+	m = A.shape[0]
+	k = A.shape[1]
+	n = B.shape[1]
+
+	initMatMulResponse = gemm_simple_pb2.MatMulInitRequest(m, k, n)
+
+	operationId = initMatMulResponse.operationId
+
+	matmul_rpc_input = createMatricesInputMessage(operationId, A, B)
+
+
+def run():
+
+	# DO IT WITH KNOWN INPUTS
+
+	data_dir = "/home/shein/Documents/grad_school/research/BigBLAS/data/"
+	A_filename, B_filename = "A_inds_32768_65536", "B_inds_65536_16384"
+	A = np.reshape(np.fromfile(data_dir + A_filename, dtype=np.float32), (32768, 65536))
+	B = np.reshape(np.fromfile(data_dir + B_filename, dtype=np.float32), (65536, 16384))
+
+    # NOTE(gRPC Python Team): .close() is possible on a channel and should be
+    # used in circumstances in which the with statement does not fit the needs
+    # of the code.
+    with grpc.insecure_channel("localhost:50051") as channel:
+        stub = gemm_simple_pb2_grpc.RouteGuideStub(channel)
+        matrixOutput = do_rpc_matmul(stub, A, B)
+    	
+    	operationId = matrixOutput.operationId
+
+    	print("Finished Operation with ID = " + str(operationId) + "\n")
+    	m, n = matrixOutput.m, matrixOutput.n
+    	print("Output Dims: (" + str(m) + ", " + str(n) + ")\n")
+    	matrixData = matrixOutput.data
+    	C = np.reshape(np.asarray(matrixData, dtype=np.float32), (m, n))
+
+
+if __name__ == "__main__":
+    logging.basicConfig()
+    run()
---
+++ clientServer/python_server/gemm_server_simple.py
@@ -0,0 +1,106 @@
+from concurrent import futures
+
+import logging
+import math
+import time
+import numpy as np
+
+import grpc
+import gemm_simple_pb2
+import gemm_simple_pb2_grpc
+
+
+def createInitMatMulResponse(operation):
+	
+	## later on do function for estimating time
+	response = gemm_pb2.MatMulInitResponse(
+		operationId = operation.id
+	)
+	return response
+
+def createMatrixOutput(operation_id, out_matrix):
+	shape = np.shape(out_matrix)
+	matrixData = np.ravel(out_matrix).tolist()
+	outputMatrix = gemm_pb2.MatrixChunk(
+		operationId=operation_id,
+		m=shape[0],
+		n=shape[1]
+	)
+
+	outputMatrix.data[:] = matrixData
+	return outputMatrix
+
+class Operation():
+
+	def __init__(self, operation_id, m, k, n, pref):
+		self.id = operation_id
+		self.M = m
+		self.K = k
+		self.N = n
+		
+
+class MatMulServicer(gemm_pb2_grpc.MatMulServicer):
+    """Provides methods that implement functionality of route guide server."""
+
+    def __init__(self):
+    	self.operationCounter = 0
+        self.operations = {}
+
+
+    def RequestMatMul(self, request, context):
+    	M, K, N = request.m, request.k, request.n
+    	# make sure this is set atomic
+    	operation_id = self.operationCounter
+    	self.operationCounter += 1
+    	newOperation = Operation(operation_id, M, K, N, pref)
+    	self.operations[operation_id] = newOperation
+
+    	initResponse = createInitMatMulResponse(newOperation)
+    	return initResponse
+
+
+    def MatrixMultiply(self, request, context):
+
+    	operationId = request.operationId
+    	m, k, n = request.m, request.k, request.n
+
+    	if request.operationId not in self.operations:
+    		#error
+    		print("Operation not granted!")
+    		return
+
+    	operation = self.operations[operationId]
+
+    	if m != operation.M or k != operation.K or n != operation.N:
+    		#error
+    		print("Operation dims do not match!")
+    		return
+
+    	data = request.data
+
+    	A = np.reshape(np.asarray(data[:m * k], dtype=np.float32), (m, k))
+    	B = np.reshape(np.asarray(data[m * k: ], dtype=np.float32), (k, n))
+
+    	C = np.matmul(A, B)
+
+    	outputMatrix = createMatrixOutput(operationId, C)
+    	return outputMatrix
+
+
+
+        
+
+
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    gemm_pb2_grpc.add_MatMulServicer_to_server(
+        MatMulServicer(), server
+    )
+    server.add_insecure_port("[::]:50051")
+    server.start()
+    server.wait_for_termination()
+
+
+if __name__ == "__main__":
+    logging.basicConfig()
+    serve()
